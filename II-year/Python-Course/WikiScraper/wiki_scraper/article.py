a7de0043aee93db18a07c4248e3fe58c5dbe24cc
Mateusz Burza <burza.mati@gmail.com>
2026-07-26 13:49:48 +0200

    II-year: Python Course Final Project - Wiki Scraper

---
+++ II-year/Python-Course/WikiScraper/wiki_scraper/article.py
@@ -0,0 +1,68 @@
+# Module containing implementation of class `Article`.
+# This class represents an article
+# and is responsible for parsing scrapped content.
+from collections import Counter
+from bs4 import BeautifulSoup
+from pandas import DataFrame, read_html
+from re import findall, sub
+from io import StringIO
+
+
+class Article:
+    def __init__(self, html_content, phrase: str):
+        self.html_content = html_content
+        self.phrase = phrase
+        self._parsed_content = BeautifulSoup(self.html_content, "html.parser")
+
+        temp_container = self._parsed_content.find(
+            "div", class_="mw-content-ltr mw-parser-output"
+        )
+
+        if temp_container is None:
+            self.container = None
+            return
+
+        # Cleans unwanted tags from the container
+        # (stored as nodes in the BeautifulSoup tree)
+        for tag in temp_container.find_all(["style", "script"]):
+            tag.decompose()
+
+        self.container = temp_container
+
+    def get_first_paragraph(self) -> str:
+        if self.container is not None:
+            paragraph = self.container.find("p")
+            if paragraph is not None:
+                text = paragraph.get_text(separator=" ", strip=True)
+
+                # Remove spaces before punctuation marks
+                return sub(r'\s+([.,!?;:)])', r'\1', text)
+        return ""
+
+    def get_table_by_index(self, index: int) -> DataFrame:
+        if self.container is None:
+            return DataFrame()
+
+        tables = self.container.find_all("table")
+
+        if index < 1 or index > len(tables):
+            return DataFrame()
+
+        nth_table = tables[index - 1]
+        # function from `pandas`
+        df = read_html(StringIO(str(nth_table)))[0]
+        return df
+
+    def count_words(self) -> dict[str, int]:
+        if self.container is None:
+            return {}
+
+        text = self.container.get_text(separator=" ", strip=True)
+
+        # regex looking for words, allowing internal apostrophes and e-acute
+        pattern = r"[A-Za-zÉé]+(?:'[A-Za-zÉé]+)*"
+        words = findall(pattern, text.lower())
+
+        # creates needed dictionary
+        return dict(Counter(words))
+        
---
+++ II-year/Python-Course/WikiScraper/wiki_scraper/controller.py
@@ -0,0 +1,64 @@
+# Module containing implementation of class `Controller`,
+# which manages the flow of the program.
+from pandas import Series
+from wiki_scraper.scraper import Scraper
+from wiki_scraper.utils import (OK, update_word_counts,
+                                analyze_relative_word_freq, auto_count_words)
+
+
+class Controller:
+    def __init__(self, args):
+        self.article = None
+        self.args = args
+        self.phrase = args.phrase
+
+    def run(self):
+        handlers = {
+            "summary": self._handle_summary,
+            "table": self._handle_table,
+            "count-words": self._handle_count_words,
+            "analyze-relative-word-frequency": self._handle_relative_word_freq,
+            "auto-count-words": self._handle_auto_count_words,
+        }
+
+        handlers[self.args.cmd]()
+        return OK
+
+    def _handle_summary(self):
+        self._ensure_article()
+        print(self.article.get_first_paragraph())
+
+    def _handle_table(self):
+        self._ensure_article()
+        index = self.args.number
+
+        df = self.article.get_table_by_index(index=index)
+        path = self.phrase + ".csv"
+        df.to_csv(path, index=False)
+        flattened_df = Series(df.values.ravel()).value_counts()
+        print(flattened_df)
+
+    def _handle_count_words(self):
+        self._ensure_article()
+        to_add = self.article.count_words()
+        update_word_counts(to_add)
+
+    def _handle_relative_word_freq(self):
+        mode = self.args.mode
+        n = self.args.count
+        chart_path = self.args.chart
+        analyze_relative_word_freq(mode=mode, n=n, chart_path=chart_path)
+
+    def _handle_auto_count_words(self):
+        start_phrase = self.phrase
+        depth = self.args.depth
+        wait = self.args.wait
+        auto_count_words(start_phrase=start_phrase, depth=depth, wait=wait)
+
+    def _ensure_article(self):
+        # Maybe without if, so as article will be refreshed each time?
+        # Then checking if article.phrase == self.phrase
+        # What if there should be two phrases in self.phrase?
+        if self.article is None and self.phrase is not None:
+            scraper = Scraper(phrase=self.phrase)
+            self.article = scraper.scrape()
